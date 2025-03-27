3dd7c1bd9745b66516f30665c3072b01b0104cf1
MS Mahadi <144730100+msmahadi@users.noreply.github.com>
2025-03-27 14:54:02 +0600

    Update 3 project-1.js

--- project-1.js
+++ project-1.js
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 export default async function handler(req, res) {
+    // MongoDB URI সরাসরি কোডে
     const uri = 'mongodb+srv://kerewev281:EwEt3GswALekuJ0x@cluster0.bj65ojg.mongodb.net/Testcomponents?retryWrites=true&w=majority';
 
     const client = new MongoClient(uri);
